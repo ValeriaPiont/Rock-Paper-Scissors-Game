10fdeeefa83e5193749ad8213a0c80e48d4b6889
Valeria Piont <iceflame.vvv@gmail.com>
2022-09-22 23:31:31 +0300

    task 2: implement Gallows game

---
+++ game2/Game.rb
@@ -0,0 +1,53 @@
+HANGMAN = %w[part0 part1 part2 part3 part4 part5 part6]
+
+max_wrong_choices = HANGMAN.length
+WORDS = %w[autumn cat]
+
+word = WORDS[rand(WORDS.length)]
+
+visible_word = '_' * word.length
+wrong_choices = 0
+used_letters = []
+
+while wrong_choices < max_wrong_choices && visible_word != word
+  puts(HANGMAN[0 .. wrong_choices])
+
+  puts("Used letters:")
+  puts(used_letters.to_s)
+  puts("Current result:")
+  puts(visible_word)
+
+  puts("Enter letter")
+  user_answer = STDIN.gets.chomp
+
+  while used_letters.include?(user_answer)
+    puts("Enter letter again")
+    user_answer = STDIN.gets.chomp
+  end
+
+  used_letters.push(user_answer)
+
+  is_exist = false
+
+  word.split("").each_with_index do |letter, index|
+          if user_answer == letter
+            visible_word[index] = user_answer
+            is_exist = true
+          end
+  end
+
+  if is_exist
+    puts("->You guessed it<-")
+  else
+    puts("->+1 step to be hanged<-")
+    wrong_choices += 1
+  end
+
+end
+
+if wrong_choices == max_wrong_choices
+  puts("You were hanged")
+  puts(word)
+elsif
+  puts("You won")
+end
