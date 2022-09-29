11817adc6fb45236928d3605fc023ae8329ed93b
Valeria Piont <iceflame.vvv@gmail.com>
2022-09-29 22:51:53 +0300

    task4: update alphabet task

--- task4/alphabet.rb
+++ task4/alphabet.rb
@@ -2,18 +2,17 @@
 ALPHABET = ('a'..'z').to_a
 
 def convert_to_number(symbol)
-  number = ALPHABET.index(symbol)
-  if number == nil
-    return symbol
-  end
-  number.to_i + 1
+  index = ALPHABET.index(symbol)
+  index + 1 if index
 end
 
-puts("Enter word: ")
+def alphabet_position(text)
+  symbols = text.downcase.split("")
+  symbols = symbols.map { |symbol| convert_to_number(symbol)}
+  symbols.compact.join(" ").to_s
+end
+
+puts "Enter word: "
 user_answer = STDIN.gets.chomp
+puts "Result: " + alphabet_position(user_answer)
 
-symbols = user_answer.downcase.split("")
-
-symbols = symbols.map { |symbol| convert_to_number(symbol)}
-
-puts "Result: " + symbols.join.to_s
